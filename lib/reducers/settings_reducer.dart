bddf18813152cdf7f1c48be538810b531cf6ec4d
Matti Ristimaki <ristimaki.matti@gmail.com>
2022-06-05 15:35:16 +0300

    Get settings from database on app start up

--- lib/reducers/settings_reducer.dart
+++ lib/reducers/settings_reducer.dart
@@ -5,6 +5,7 @@ import 'package:redux/redux.dart';
 Reducer<SettingsState> settingsReducer = combineReducers([
   TypedReducer(_updateUsingRestingTime),
   TypedReducer(_updateRestingTimeSeconds),
+  TypedReducer(_getRestingTimeSettings),
 ]);
 
 SettingsState _updateUsingRestingTime(
@@ -14,3 +15,9 @@ SettingsState _updateUsingRestingTime(
 SettingsState _updateRestingTimeSeconds(
         SettingsState state, UpdateRestingTimeInSecondsAction action) =>
     state.copyWith(restingTimeInSeconds: action.seconds);
+
+SettingsState _getRestingTimeSettings(
+        SettingsState state, GetSettingsAction action) =>
+    state.copyWith(
+        restingTimeInSeconds: action.restingTimeInSeconds,
+        usingRestingTime: action.usingRestingTime);
